aefb702c3a1e3e6a8e9c967e885259d56016ba8d
kk <lyvanna544@gmail.com>
2025-06-20 14:32:59 +0700

    udpate userservice

--- userservice/userserviceimpl.cpp
+++ userservice/userserviceimpl.cpp
@@ -1,8 +1,14 @@
 #include<iostream> 
 using namespace std; 
 
+class UserService{
+    private: 
+        int id; 
+        string name; 
+        string gender; 
+}
 int main(){
-
+    cout<<"Another another line in here !"<<endl; 
     cout<<"User service impl "<<endl; 
     return 0; 
 }
